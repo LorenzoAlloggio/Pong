450a98405b255b80eb37db36207037d87b4c458d
LorenzoAlloggio <l.alloggio@sintlucasedu.nl>
2023-09-28 12:43:54 +0200

    Update Ball.cs

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    public float Xposition = 0f;
+    public float Yposition = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(Xposition, Yposition, 0);
+
     }
     
     // Update is called once per frame
     void Update()
     {
-        
+        Xposition = Xposition + 0.01f;
+        transform.position = new Vector3(Xposition, Yposition, 0);
     }
 }
