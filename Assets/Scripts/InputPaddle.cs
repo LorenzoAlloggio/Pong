ba2caf5f7dd2098925d95143ffd50d217e230171
LorenzoAlloggio <l.alloggio@sintlucasedu.nl>
2023-10-05 12:22:57 +0200

    update

---
+++ Assets/Scripts/InputPaddle.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class InputPaddle : MonoBehaviour
+{
+    public float speed = 3f;
+    public string leftOrRight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (leftOrRight == "Left")
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(Vector3.up * speed * Time.deltaTime);
+                Debug.Log("yes W is pressed down");
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(Vector3.down * speed * Time.deltaTime);
+                Debug.Log("yes S is pressed down");
+            }
+        }
+        else if(leftOrRight == "Right")
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.Translate(Vector3.up * speed * Time.deltaTime);
+                Debug.Log("yes UpArrow is pressed down");
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.Translate(Vector3.down * speed * Time.deltaTime);
+                Debug.Log("yes DownArrow is pressed down");
+            }
+        }
+    }
+}
