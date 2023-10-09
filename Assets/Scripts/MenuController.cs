a260009d74d68dee61113e17984d7ec3e8ba2f03
LorenzoAlloggio <l.alloggio@sintlucasedu.nl>
2023-10-09 14:00:47 +0200

    update

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+
+    public Button Play_Button;
+    public Button Quit_Button;
+
+    public void Play_Game()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit_game()
+    {
+        Application.Quit();
+    }
+}
