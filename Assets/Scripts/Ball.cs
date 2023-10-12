2bade4817852162f70133ebd963462a4e4a38ff9
LorenzoAlloggio <l.alloggio@sintlucasedu.nl>
2023-10-12 10:24:41 +0200

    update

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,30 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Threading;
-using System.Threading.Tasks;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Ball : MonoBehaviour
-
 {
     public AudioClip BallHitSound;
     private AudioSource audioSource;
-    public Text Scoreboard;
+    public TMP_Text Scoreboard;
     private int Paddle_1_Score = 0;
     private int Paddle_2_Score = 0;
-    public float Xposition = 0f;
-    public float Yposition = 0f;
+    private float Xposition = 0f;
+    private float Yposition = 0f;
     public float Xspeed;
     public float Yspeed;
+
+    private Vector3 initialPosition; // Store the initial position of the ball.
+    private float initialXSpeed;
+    private float initialYSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        //transform.position = new Vector3(Xposition, Yposition, 0);
-        Xspeed = 5f;
-        Yspeed = 5f;
+        initialPosition = new Vector3(Xposition, Yposition, 0);
+        initialXSpeed = Xspeed;
+        initialYSpeed = Yspeed;
         audioSource = gameObject.AddComponent<AudioSource>();
         audioSource.clip = BallHitSound;
     }
@@ -41,35 +42,22 @@ public class Ball : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("HorizontalWall"))
-
         {
-            Yspeed = Yspeed * -1;
-            audioSource.Play();            
+            Yspeed = -Yspeed;
+            audioSource.Play();
         }
 
         if (collision.gameObject.CompareTag("VerticalWall"))
         {
             Paddle_2_Score++;
-            Xposition = 0;
-            Yposition = 0;
-            Yspeed = 0;
-            Xspeed = 0;
-            Yspeed = 5f;
-            Xspeed = 5f;
+            ResetBall();
             audioSource.Play();
         }
-
-
 
         if (collision.gameObject.CompareTag("VerticalWall2"))
         {
             Paddle_1_Score++;
-            Xposition = 0;
-            Yposition = 0;
-            Yspeed = 0;
-            Xspeed = 0;
-            Yspeed = 5f;
-            Xspeed = 5f;
+            ResetBall();
             audioSource.Play();
         }
 
@@ -87,12 +75,17 @@ public class Ball : MonoBehaviour
 
         if (collision.gameObject.CompareTag("Paddle"))
         {
-            Xspeed = Xspeed * -1;
+            Xspeed = -Xspeed * 1.1f;
             audioSource.Play();
         }
-
-
     }
 
-
+    // Function to reset the ball to its initial state.
+    private void ResetBall()
+    {
+        Xposition = initialPosition.x;
+        Yposition = initialPosition.y;
+        Xspeed = initialXSpeed;
+        Yspeed = initialYSpeed;
+    }
 }
--- Assets/Scripts/InputPaddle.cs
+++ Assets/Scripts/InputPaddle.cs
@@ -5,12 +5,13 @@ using UnityEngine.UIElements;
 
 public class InputPaddle : MonoBehaviour
 {
-    public float speed = 3f;
+    public float speed = 8f;
     public string leftOrRight;
     // Start is called before the first frame update
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -21,25 +22,32 @@ public class InputPaddle : MonoBehaviour
             if (Input.GetKey(KeyCode.W))
             {
                 transform.Translate(Vector3.up * speed * Time.deltaTime);
-                Debug.Log("yes W is pressed down");
             }
             else if (Input.GetKey(KeyCode.S))
             {
                 transform.Translate(Vector3.down * speed * Time.deltaTime);
-                Debug.Log("yes S is pressed down");
             }
         }
-        else if(leftOrRight == "Right")
+        else if (leftOrRight == "Right")
         {
             if (Input.GetKey(KeyCode.UpArrow))
             {
                 transform.Translate(Vector3.up * speed * Time.deltaTime);
-                Debug.Log("yes UpArrow is pressed down");
             }
             else if (Input.GetKey(KeyCode.DownArrow))
             {
                 transform.Translate(Vector3.down * speed * Time.deltaTime);
-                Debug.Log("yes DownArrow is pressed down");
+            }
+            else if (leftOrRight == "Right")
+            {
+                if (Input.GetKey(KeyCode.UpArrow))
+                {
+                    transform.Translate(Vector3.up * speed * Time.deltaTime);
+                }
+                else if (Input.GetKey(KeyCode.DownArrow))
+                {
+                    transform.Translate(Vector3.down * speed * Time.deltaTime);
+                }
             }
         }
     }
